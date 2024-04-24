c898bd431a69384f96a80e7ec420c9a2eb1c960b
Magnus Meseck <magnus.meseck@googlemail.com>
2024-04-25 01:24:38 +0700

    Fix types:
    - Previously, mixed parameterless and optionally-parametered constrcutors were recognized as having a required parameter in the Fusion.

--- source/types.ts
+++ source/types.ts
@@ -4,13 +4,41 @@ export type FusedClass<Classes extends any[]> =
 type FusedConstructorParameterSet<Classes extends (abstract new (arg?: any) => any)[]> =
     Classes extends ([infer CurrentClass extends new (arg?: any) => any, ...infer FollowingClasses extends (abstract new (arg?: any) => any)[]]) ? [...ConstructorParameters<CurrentClass>, ...FusedConstructorParameterSet<FollowingClasses>] : [];
 
-type WithoutOptionals<Tuple extends any[]> =
-    [Tuple] extends [[infer CurrentTupleMember, ...infer FollowingTupleMembers]] ? Extract<CurrentTupleMember, undefined> extends never ? [CurrentTupleMember, ...WithoutOptionals<FollowingTupleMembers>] : [...WithoutOptionals<FollowingTupleMembers>] : [];
 
-type ConstructorParamsOptional<Tuple extends any[]> =
-    WithoutOptionals<Tuple> extends [] ? true : false;
+type AllConstructorParamsOptional<Classes extends (abstract new (arg?: any) => any)[]> =
+    AllTupleMembersExtendType<
+        AllOptional<
+            ConstructParamsOf<Classes>
+        >
+    , true
+    >;
 
-export type FusedConstructor<Classes extends (abstract new (arg?: any) => any)[]> = 
-    ConstructorParamsOptional<Classes> extends true ?
-    new (args?: FusedConstructorParameterSet<Classes>) => FusedClass<Classes>:
+type ConstructParamsOf<T extends any[]> = {
+    [K in keyof T]: ConstructorParameters<T[K]>;
+};
+
+type AllOptional <T extends any[][]> = {
+    [K in keyof T]: 
+        T[K] extends [] ?
+            true:
+            AllTupleMembersIncludeType<T[K], undefined>
+};
+
+type AllTupleMembersExtendType<Tuple extends any[], ConditionType> =
+    Tuple extends [infer First, ...infer Rest] ?
+    First extends ConditionType ?
+        AllTupleMembersExtendType<Rest, ConditionType>
+        : false
+    : true;
+
+type AllTupleMembersIncludeType<Tuple extends any[], ConditionType> =
+    Tuple extends [infer First, ...infer Rest] ?
+    ConditionType extends First ?
+        AllTupleMembersIncludeType<Rest, ConditionType>
+        : false
+    : true;
+
+export type FusedConstructor<Classes extends (abstract new (arg?: any) => any)[]> =
+    AllConstructorParamsOptional<Classes> extends true ?
+    new (args?: FusedConstructorParameterSet<Classes>) => FusedClass<Classes> :
     new (args: FusedConstructorParameterSet<Classes>) => FusedClass<Classes>;
